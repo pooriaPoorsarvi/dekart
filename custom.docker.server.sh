b00a3bc35d63499c7104839e345cdedcd1accdf3
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2023-03-10 17:06:17 -0500

    custom server command added

--- custom.docker.server.sh
+++ custom.docker.server.sh
@@ -2,8 +2,19 @@
 
 
 
+/dekart/cloud-sql-proxy ${INSTANCE_CONNECTION_NAME} > output.txt &
+timeout=10
+while ! grep -q "The proxy has started" output.txt; do
+  sleep 1
+  timeout=$((timeout - 1))
+  if [ $timeout -eq 0 ]; then
+    echo "Error: postgres could not be reached"
+    exit 1
+  fi
+done
+/dekart/server
 
-/dekart/cloud-sql-proxy ${INSTANCE_CONNECTION_NAME} & /dekart/server
+
 
 
 
