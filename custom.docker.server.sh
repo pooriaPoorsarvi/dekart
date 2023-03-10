8ceeb80df61917945e037be968cef088e6db1449
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2023-03-10 11:29:34 -0500

    fixed SA issues

--- custom.docker.server.sh
+++ custom.docker.server.sh
@@ -3,10 +3,7 @@
 
 
 
-gcloud auth login --cred-file=$GOOGLE_APPLICATION_CREDENTIALS
-gcloud config set project ppt-personal-project
-
-./cloud-sql-proxy ppt-personal-project:us-central1:dekart & ./server
+/dekart/cloud-sql-proxy ppt-personal-project:us-central1:dekart & /dekart/server
 
 
 
